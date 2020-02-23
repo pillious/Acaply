caca1c92454bf3b12cbcf2db49de63b02d32dbc7
Andrew <andrewzhlee@gmail.com>
2020-02-23 16:10:35 -0500

    username must be alphanumeric

--- public/js/signup.js
+++ public/js/signup.js
@@ -5,14 +5,31 @@ var errorMsgElement = document.getElementById("login-error-msg");
 
 // signup on btn press
 $("#submit-signup").click(function () {
+    validateSignup();
+});
+
+// signup on enter key press
+$('#username, #password, #email').keypress(function (event) {
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == '13') {
+        validateSignup();
+    }
+});
+
+function validateSignup() {
     if (passwordElement.value != '' && usernameElement.value != '' && emailElement.value != '') {
         var isEmailValid = emailIsValid(emailElement.value);
 
         if (passwordElement.value != '' && usernameElement.value != '') {
             if (isEmailValid) {
-                signup();
-            }
-            else {
+                // username must be alphanumeric
+                if (/^[0-9a-zA-Z]+$/.test(usernameElement.value)) {
+                    signup();
+                } else {
+                    errorMsgElement.style.display = "block";
+                    errorMsgElement.innerHTML = "Usernames must consist of only numbers and letters.";
+                }
+            } else {
                 errorMsgElement.style.display = "block";
                 errorMsgElement.innerHTML = "Invalid email.";
             }
@@ -21,41 +38,7 @@ $("#submit-signup").click(function () {
         errorMsgElement.style.display = "block";
         errorMsgElement.innerHTML = "All fields are required.";
     }
-});
-
-// signup on enter key press
-$('#username').keypress(function (event) {
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-
-    if (keycode == '13' && passwordElement.value != '' && usernameElement.value != '') {
-        var isEmailValid = emailIsValid(emailElement.value);
-
-        if (isEmailValid) {
-            signup();
-        }
-        else {
-            errorMsgElement.style.display = "block";
-            errorMsgElement.innerHTML = "Invalid email.";
-        }
-    }
-});
-
-$('#password').keypress(function (event) {
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-
-    if (keycode == '13' && passwordElement.value != '' && usernameElement.value != '') {
-        var isEmailValid = emailIsValid(emailElement.value);
-
-        if (isEmailValid) {
-            signup();
-        }
-        else {
-            errorMsgElement.style.display = "block";
-            errorMsgElement.innerHTML = "Invalid email.";
-        }
-    }
-
-});
+}
 
 // returns true/false depending on if email is valid
 function emailIsValid(email) {
@@ -79,8 +62,7 @@ async function signup() {
         if (signupData.data.errorCode === 11000) {
             if (signupData.data.errorMsg.includes("username")) {
                 errorMsgElement.innerHTML = "Sorry, this username is already taken.";
-            }
-            else if (signupData.data.errorMsg.includes("email")) {
+            } else if (signupData.data.errorMsg.includes("email")) {
                 errorMsgElement.innerHTML = "Email already in use.";
             }
         } else {
