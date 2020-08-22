714a16371f8daf51fc4160da4a6aab48f6ea6f69
Jacob C <45279415+jacobc2700@users.noreply.github.com>
2020-08-21 21:53:26 -0400

    lower comment count

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,72 +1,77 @@
 const mongoose = require('mongoose');
 const CommentSchema = require('../models/Comment');
 
-const {
-    PostClass,
-    CommentClass,
-    UserClass
-} = require('../modules/modules');
+const { PostClass, CommentClass, UserClass } = require('../modules/modules');
 
 //Create a new comment.
 exports.create_comment = async function (req, resp) {
-    if (req.session.userSessionId) {
-        // get the user doc (used to get author's id)
-        const userClassInstance = new UserClass();
-        var userDoc = await userClassInstance.getUserProfileBySession(req.session.userSessionId);
+  if (req.session.userSessionId) {
+    // get the user doc (used to get author's id)
+    const userClassInstance = new UserClass();
+    var userDoc = await userClassInstance.getUserProfileBySession(
+      req.session.userSessionId
+    );
 
-        const postId = req.body.params.postId;
-        const commentBody = req.body.params.comment;
+    const postId = req.body.params.postId;
+    const commentBody = req.body.params.comment;
 
-        // create the comment & save to db
-        const commentClassInstance = new CommentClass();
-        await commentClassInstance.createNewComment(commentBody, postId, userDoc);
+    // create the comment & save to db
+    const commentClassInstance = new CommentClass();
+    await commentClassInstance.createNewComment(commentBody, postId, userDoc);
 
-        resp.status(200).send("new comment success!");
-    } else {
-        resp.status(400).send("new comment failed");
-    }
+    resp.status(200).send('new comment success!');
+  } else {
+    resp.status(400).send('new comment failed');
+  }
 };
 
 //Edit an existing comment
 exports.edit_comment = async function (req, resp) {
-    if (req.session.userSessionId) {
-        // get the user doc (used to get author's id)
-        const userClassInstance = new UserClass();
-        var userDoc = await userClassInstance.getUserProfileBySession(req.session.userSessionId);
+  if (req.session.userSessionId) {
+    // get the user doc (used to get author's id)
+    const userClassInstance = new UserClass();
+    var userDoc = await userClassInstance.getUserProfileBySession(
+      req.session.userSessionId
+    );
 
-        const commentId = req.body.params.commentId;
-        const commentBody = req.body.params.commentBody.trim();
+    const commentId = req.body.params.commentId;
+    const commentBody = req.body.params.commentBody.trim();
 
-        // save the edited comment to db
-        const commentClassInstance = new CommentClass();
-        const commentDoc = await commentClassInstance.getComment(commentId);
+    // save the edited comment to db
+    const commentClassInstance = new CommentClass();
+    const commentDoc = await commentClassInstance.getComment(commentId);
 
-        // check if original comment author is the same as the user who edited the comment 
-        if (commentDoc.author.toString() === userDoc._id.toString()) {
-
-            // update edit to db
-            var updatedCommentDoc = await commentClassInstance.editComment(commentId, commentBody);
-            (updatedCommentDoc) ? (resp.status(200).send("edit comment success!")) : (resp.status(400).send("edit comment failed"))
-            
-        }
-        else {
-            resp.status(400).send("edit comment failed");
-        }   
-
+    // check if original comment author is the same as the user who edited the comment
+    if (commentDoc.author.toString() === userDoc._id.toString()) {
+      // update edit to db
+      var updatedCommentDoc = await commentClassInstance.editComment(
+        commentId,
+        commentBody
+      );
+      updatedCommentDoc
+        ? resp.status(200).send('edit comment success!')
+        : resp.status(400).send('edit comment failed');
     } else {
-        resp.status(400).send("edit comment failed");
+      resp.status(400).send('edit comment failed');
     }
+  } else {
+    resp.status(400).send('edit comment failed');
+  }
 };
 
 //Delete a comment.
 exports.delete_comment = async function (req, resp) {
-    if (req.session.userSessionId) {
-        const commentClassInstance = new CommentClass();
-        var commentDoc = await commentClassInstance.deleteComment(req.params.commentId);
+  if (req.session.userSessionId) {
+    const commentClassInstance = new CommentClass();
+    var commentDoc = await commentClassInstance.deleteComment(
+      req.params.commentId
+    );
+    // console.log(commentDoc);
+    await commentClassInstance.incrementCommentCount(commentDoc.parentPost);
 
-        resp.send(commentDoc);
-    } else {
-        //Redirect the user to the home page.
-        resp.redirect('/')
-    }
-}
+    resp.send(commentDoc);
+  } else {
+    //Redirect the user to the home page.
+    resp.redirect('/');
+  }
+};
